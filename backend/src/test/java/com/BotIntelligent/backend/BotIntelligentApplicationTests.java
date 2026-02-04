f9f410420cbf8c14d41edc7300a43e999f2fba3a
MOMEOUATT <emmanuelmarieouattara@gmail.com>
2026-02-04 03:40:53 +0100

    Initial commit - Configuration du projet Bot Intelligent

---
+++ backend/src/test/java/com/BotIntelligent/backend/BotIntelligentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BotIntelligent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotIntelligentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
