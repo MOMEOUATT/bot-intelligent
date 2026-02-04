9ba71835ca8f8ee961362df1fd7426917ee014a9
MOMEOUATT <emmanuelmarieouattara@gmail.com>
2026-02-04 03:40:53 +0100

    Initial commit - Configuration du projet Bot Intelligent

---
+++ backend/src/main/java/com/BotIntelligent/backend/BotIntelligentApplication.java
@@ -0,0 +1,13 @@
+package com.BotIntelligent.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotIntelligentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotIntelligentApplication.class, args);
+	}
+
+}
